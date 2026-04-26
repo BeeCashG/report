2dc4ead16f58b8f0c2fe0f4545c2bfd6dc443dbb
BeeCashG <gbikash34@gmail.com>
2026-04-26 20:00:54 +0545

    Fix build crash on cPanel by disabling Turbopack and symlinks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Force Webpack and disable Turbopack build features
+  experimental: {
+    // @ts-ignore - disabling turbo build
+    turbo: {
+      enabled: false
+    }
+  },
+  // Ensure we don't use symlinks for the build process
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+    return config;
+  }
 };
 
 export default nextConfig;
